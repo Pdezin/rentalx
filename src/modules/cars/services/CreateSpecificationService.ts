e208d0ef9559cc2f002a4878b4a538335cec356d
Pedro Jardim <pedro200219@outlook.com>
2022-05-14 12:11:08 -0300

    Implementation structure useCase

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,21 @@
-class CreateSpecificationService {
-  execute() {
-    console.log("Test");
-  }
+import { SpecificationsRepository } from "../repositories/SpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
 }
 
+class CreateSpecificationService {
+  constructor(private specificationRepository: SpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationsAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationsAlreadyExists)
+      throw new Error("Specification already exists");
+
+    this.specificationRepository.create({ name, description });
+  }
+}
 export { CreateSpecificationService };
