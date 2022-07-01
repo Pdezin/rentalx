b99849f42f3495fb7a6e43632beaf2efe63e7c7d
Pedro Jardim <pedro200219@outlook.com>
2022-06-30 21:15:29 -0300

    SES Mail Provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,29 +1,5 @@
-import { container } from "tsyringe";
+// Register providers here, this document is used in the index container
 
-import { IDateProvider } from "./DateProvider/IDateProvider";
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
-import { IMailProvider } from "./MailProvider/IMailProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
-import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
-import { IStorageProvider } from "./StorageProvider/IStorageProvider";
-
-container.registerSingleton<IDateProvider>(
-  "DayjsDateProvider",
-  DayjsDateProvider
-);
-
-container.registerInstance<IMailProvider>(
-  "EtherealMailProvider",
-  new EtherealMailProvider()
-);
-
-const diskStorage = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-  "StorageProvider",
-  diskStorage[process.env.disk]
-);
+import "./DateProvider";
+import "./StorageProvider";
+import "./MailProvider";
